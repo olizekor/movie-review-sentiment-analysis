cf7d411462018c8097d48633242ad78e9da5f0b8
olizekor <134792856+olizekor@users.noreply.github.com>
2025-06-24 19:46:36 +0100

    modified dataloader, EDA notebook

--- src/data_loader.py
+++ src/data_loader.py
@@ -2,7 +2,9 @@ import pandas as pd
 import os
 
 def load_raw_data(filename="IMDB_Dataset.csv"):
-    filepath = os.path.join("data", "raw", filename)
+    # Go up one directory from notebooks/ → root, then into data/raw/
+    base_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+    filepath = os.path.join(base_dir, "data", "raw", filename)
     return pd.read_csv(filepath)
 
 # def load_processed_data(filename="cleaned_reviews.csv"):
